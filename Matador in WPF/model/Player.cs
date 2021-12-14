0bdafaea46f96039ab680523a6b5528a65477d47
jacob <jacobriis86@hotmail.com>
2021-12-14 15:18:32 +0100

    Initial commit

---
+++ Matador in WPF/model/Player.cs
@@ -0,0 +1,23 @@
+﻿namespace Matador_in_WPF.model;
+
+public class Player
+{
+    private string _name;
+    private int _playerNumber;
+    private int _walletAmount;
+    private int _position;
+    private int _currentRoll;
+    private bool _inJail;
+
+    private Player(string name, int playerNumber, int walletAmount, int position, bool inJail, int currentRoll)
+    {
+        this._name = name;
+        this._playerNumber = playerNumber;
+        this._walletAmount = walletAmount;
+        this._position = position;
+        this._inJail = inJail;
+        this._currentRoll = currentRoll;
+    }
+    
+    
+}
