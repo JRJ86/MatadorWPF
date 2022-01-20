a37a4b44ce561a93d43a937f85d5cde261279169
jacob <jacobriis86@hotmail.com>
2022-01-20 11:17:29 +0100

    clean up

--- Matador in WPF/obj/Debug/netcoreapp3.1/App.g.cs
+++ Matador in WPF/obj/Debug/netcoreapp3.1/App.g.cs
@@ -44,7 +44,7 @@ namespace Matador_in_WPF {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -59,7 +59,7 @@ namespace Matador_in_WPF {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
         public static void Main() {
             Matador_in_WPF.App app = new Matador_in_WPF.App();
             app.InitializeComponent();
